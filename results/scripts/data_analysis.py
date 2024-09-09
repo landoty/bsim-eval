55574805062a16e3a6d0bf0aa5ea14d63ef676da
Landen Doty <landen.doty@outlook.com>
2024-09-08 22:36:10 -0500

    updated output parsing

--- results/scripts/data_analysis.py
+++ results/scripts/data_analysis.py
@@ -1,8 +1,9 @@
 import sys
 import os
+import re
 import matplotlib.pyplot as plt
 
-colors = ['dodgerblue', 'forestgreen', 'black']
+colors = ['dodgerblue', 'forestgreen', 'mediumorchid', 'black']
 def evaluate(files: list) -> None:
     fig, axs = plt.subplots(1, 2, tight_layout=False)
     table_stats = {"runs": [], "row_labels": ["Avg. Similarity", "No. of Functions", "No. of Matches", "Accuracy"], \
@@ -11,30 +12,43 @@ def evaluate(files: list) -> None:
         with open(f, "r") as file:
             sims = []
             confs = []
-            total = 0
+            no_matches = 0
+            no_fns = 0
 
             db, binary, sim_thresh, conf_thresh = file.readline().strip().split(",")
             for l in file.readlines():
-                l = l.strip()
-                try: # most lines of this form
-                    db_fn, match_fn, sim, conf = l.split(",")
-                except: # last line has the total number of functions
-                    num = int(l)
+                try:
+                    split = l.strip().split(" ")
+                    if len(split) > 1:
+                        no_fns = int(split[0])
+                        match = split[1]
+                        matched = False # new group, reset 
+                    elif not matched:
+                        match = split[0]
+                    else:
+                        continue # already found a match, continue
+
+                    db_fn, match_fn, sim, conf = re.split(r',(?=")', match)
+
+                except Exception as e:
+                    print(e)
+                    continue
 
                 if db_fn == match_fn:
-                    sims.append(float(sim))
-                    confs.append(float(conf))
-                    total += 1
+                    sims.append(float(sim.replace('"', "")))
+                    confs.append(float(conf.replace('"', "")))
+                    no_matches += 1
+                    matched = True # found a match in the group
 
             run_name = os.path.basename(f).split(".")[0]
 
             table_stats["runs"].append(run_name)
             table_stats["data"]["avg_sim"].append(round(sum(sims)/len(sims), 4))
-            table_stats["data"]["no_fns"].append(num)
-            table_stats["data"]["no_matches"].append(total)
-            table_stats["data"]["acc"].append(round(total/num, 4))
+            table_stats["data"]["no_fns"].append(no_fns)
+            table_stats["data"]["no_matches"].append(no_matches)
+            table_stats["data"]["acc"].append(round(no_matches/no_fns, 4))
 
-            axs[0].hist(sims, alpha=0.7, histtype='bar', color=colors[i], label=run_name, bins=10, range=(0.0, 1.0))
+            axs[0].hist(sims, alpha=0.7, color=colors[i], label=run_name, bins=10, range=(0.0, 1.0))
             axs[1].scatter(sims, confs, color=colors[i], alpha=0.7)
 
         axs[0].set_xlabel("similarity")
